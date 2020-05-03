18ec3088eef6360bf6544aab71bf2c783d24fec0
Nava_mystic <active99raj@gmail.com>
2020-05-03 16:41:57 +1000

    initial commit

---
+++ day.js
@@ -0,0 +1,13 @@
+var d = new Date();
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+document.getElementById("currentDate").innerHTML = 
+	days[d.getDay()] +
+	" "+ d.getDate() + " "+
+ 	months[d.getMonth()] +
+ 	 "  " + d.getFullYear() +"<br>"+
+ 	 d.getHours() + " :"+
+ 	 d.getMinutes() + " :"+
+ 	 d.getSeconds();
